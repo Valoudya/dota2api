9ce2a5d6dd69143861d32d50fa2bcbbe68174204
Valoudya <vmisostishkhov@gmail.com>
2023-07-31 22:23:20 +0300

    Added new interfaces and changed MatchInfo.tsx

--- src/components/MatchInfo.tsx
+++ src/components/MatchInfo.tsx
@@ -1,8 +1,9 @@
 import TeamCard from "./TeamCard.tsx";
+import {matchesInterface} from "../interfaces/heroesIntarface.ts";
 
-const MatchInfo = (props) => {
-
-    let time = props.matchData.duration,
+const MatchInfo = (props: matchesInterface) => {
+    const {matchData}: matchesInterface = props
+    const time = matchData.duration,
         duration = {
             minutes: Math.floor(time/60).toString().padStart(2, '0'),
             seconds: (time % 60).toString().padStart(2, '0')
@@ -13,16 +14,16 @@ const MatchInfo = (props) => {
             <div className="match-result h-[150px] w-[1000px] max-w-full items-center grid grid-cols-3">
                 <div className="radiant-team">
                     <p className="text-[32px] text-[#66bb6a]">
-                        {props.matchData.radiant_win && '👑'}
-                        {props.matchData.radiant_team_id ?
-                            props.matchData.radiant_team.name : 'Radiant'}
+                        {matchData.radiant_win && '👑'}
+                        {matchData.radiant_team_id ?
+                            matchData.radiant_team.name : 'Radiant'}
                     </p>
                 </div>
                 <div className="teams-score flex justify-center text-[32px] flex-col items-center">
                     <p className="space-x-2">
-                        <span className="radiant-score text-[#66bb6a]">{props.matchData.radiant_score}</span>
+                        <span className="radiant-score text-[#66bb6a]">{matchData.radiant_score}</span>
                         <span>:</span>
-                        <span className="dire-score text-[#bb6666]">{props.matchData.dire_score}</span>
+                        <span className="dire-score text-[#bb6666]">{matchData.dire_score}</span>
                     </p>
                     <div className="duration">
                         <p className="text-[18px]">{`${duration.minutes}:${duration.seconds}`}</p>
@@ -30,21 +31,21 @@ const MatchInfo = (props) => {
                 </div>
                 <div className="dire-team flex justify-end">
                     <p className="text-[32px] text-[#bb6666]">
-                        {!props.matchData.radiant_win && '👑'}
-                        {props.matchData.dire_team_id ?
-                            props.matchData.dire_team.name : 'Dire'}
+                        {!matchData.radiant_win && '👑'}
+                        {matchData.dire_team_id ?
+                            matchData.dire_team.name : 'Dire'}
                     </p>
                 </div>
             </div>
             {
-                props.matchData.replay_url && <a href={props.matchData.replay_url} className="px-4 py-1 border-2 border-[#202020] justify-center mt-2 rounded-full flex items-center">
+                matchData.replay_url && <a href={matchData.replay_url} className="px-4 py-1 border-2 border-[#202020] justify-center mt-2 rounded-full flex items-center">
                     Replay
                     <img src="../../public/download.png" alt="" className="w-[20px] ml-2"/>
                 </a>
             }
             <div className="teams w-full">
-                <TeamCard teamData={props.matchData.players.slice(0, 5)}/>
-                <TeamCard teamData={props.matchData.players.slice(5, 10)}/>
+                <TeamCard teamData={matchData.players.slice(0, 5)}/>
+                <TeamCard teamData={matchData.players.slice(5, 10)}/>
             </div>
         </div>
     );
--- src/interfaces/heroesIntarface.ts
+++ src/interfaces/heroesIntarface.ts
@@ -23,10 +23,25 @@ export interface matchesInterface {
     matchData: {
         match_id: number;
         league_name? : string;
-        radiant_team? : string;
+        radiant_team? : string | {
+            name?: string | undefined;
+        }
         radiant_win? : string;
         radiant_name? : string;
+        radiant_score? : number;
+        dire_team? : string | {
+            name?: string | undefined;
+        }
         dire_name? : string;
+        dire_score? : number;
+        dire_team_id? : number | undefined;
         duration? : number;
+        radiant_team_id? : number | undefined;
+        replay_url? : string | null;
+        players? : [
+            {
+                player_slot: number
+            }
+        ]
     }
 }
